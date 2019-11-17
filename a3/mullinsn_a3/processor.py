107f33780b09c91ed6be0c4d2ec604b11718f403
NolanMullins <nolan.mullins@me.com>
2019-11-17 16:04:11 -0500

    Stemming and stop word removal

--- a3/mullinsn_a3/processor.py
+++ a3/mullinsn_a3/processor.py
@@ -12,9 +12,9 @@
 import sys
 import os
 import statistics
-from multiprocessing import Process
-
+import nltk
 import numpy as np
+import string
 
 import sklearn.model_selection as mSelection
 import sklearn.feature_extraction.text as extraction
@@ -34,9 +34,16 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import KNeighborsClassifier
 
+from nltk import word_tokenize          
+from nltk.stem.porter import PorterStemmer
 
+#Stemming
+stemmer = PorterStemmer()
+analyzer = CountVectorizer(stop_words='english').build_analyzer()
+#nltk.download('punkt')
 
-#def featureSelection():
+def stemmed_words(doc):
+    return (stemmer.stem(w) for w in analyzer(doc))
 
 def analyzeDataSet(dataSet):
     print(len(dataSet['data']))
@@ -83,12 +90,77 @@ def splitData(data):
     training = {'data': x_alt, 'score': y_alt}
     return {'val': validation, 'trn': training}
 
-if __name__ == "__main__":
+def runModified(split):
+    print("\nCount Vect -> fSelect -> KNearest")
+    for i in range(1,7):
+        pipe = Pipeline([
+            ('vect', extraction.CountVectorizer(ngram_range=(1,2))),
+            ('chi2', fSelection.SelectKBest(fSelection.chi2, k=500*i)),
+            ('clf', KNeighborsClassifier()),
+        ])
+        runTest(pipe, split, str(i*500))
 
-    data = loadFiles()
-    split = splitData(data)
-    proc = []
+    print("\nCount Vect -> SelectFpr -> KNearest")
+    a = 0.0005
+    for i in range(1,7):
+        if (i%2==1):
+            a = a * 2
+        else:
+            a = a * 5
+        pipe = Pipeline([
+            ('vect', extraction.CountVectorizer(ngram_range=(1,2))),
+            ('chi2', fSelection.SelectFpr(fSelection.chi2, alpha=a)),
+            ('clf', KNeighborsClassifier()),
+        ])
+        runTest(pipe, split, "alpha = " + str(a))
 
+    print("\nCount Vect -> fSelect -> MultiNB")
+    for i in range(1,7):
+        pipe = Pipeline([
+            ('vect', extraction.CountVectorizer(ngram_range=(1,2))),
+            ('chi2', fSelection.SelectKBest(fSelection.chi2, k=500*i)),
+            ('clf', MultinomialNB()),
+        ])
+        runTest(pipe, split, str(500*i))
+
+    print("\nCount Vect -> SelectFpr -> MultiNB")
+    a = 0.0005
+    for i in range(1,7):
+        if (i%2==1):
+            a = a * 2
+        else:
+            a = a * 5
+        pipe = Pipeline([
+            ('vect', extraction.CountVectorizer(ngram_range=(1,2))),
+            ('chi2', fSelection.SelectFpr(fSelection.chi2, alpha=a)),
+            ('clf', MultinomialNB()),
+        ])
+        runTest(pipe, split, "alpha = " + str(a))
+
+    print("\nCount Vect -> fSelect -> MLP")
+    for i in range(1,7):
+        pipe = Pipeline([
+            ('vect', extraction.CountVectorizer(ngram_range=(1,2))),
+            ('chi2', fSelection.SelectKBest(fSelection.chi2, k=500*i)),
+            ('clf', neural.MLPClassifier(solver='adam',early_stopping=True, max_iter=400)),
+        ])
+        runTest(pipe, split, str(500*i))
+
+    print("\nCount Vect -> SelectFpr -> MLP")
+    a = 0.0005
+    for i in range(1,7):
+        if (i%2==1):
+            a = a * 2
+        else:
+            a = a * 5
+        pipe = Pipeline([
+            ('vect', extraction.CountVectorizer(ngram_range=(1,2))),
+            ('chi2', fSelection.SelectFpr(fSelection.chi2, alpha=a)),
+            ('clf', neural.MLPClassifier(solver='adam',early_stopping=True, max_iter=400)),
+        ])
+        runTest(pipe, split, "alpha = " + str(a))
+
+def runUnmodified(split):
     print("\nCount Vect -> fSelect -> KNearest")
     for i in range(1,7):
         pipe = Pipeline([
@@ -140,7 +212,7 @@ if __name__ == "__main__":
         pipe = Pipeline([
             ('vect', extraction.CountVectorizer()),
             ('chi2', fSelection.SelectKBest(fSelection.chi2, k=500*i)),
-            ('clf', neural.MLPClassifier(solver='adam',early_stopping=True, max_iter=400)),
+            ('clf', neural.MLPClassifier()),
         ])
         runTest(pipe, split, str(500*i))
 
@@ -154,10 +226,23 @@ if __name__ == "__main__":
         pipe = Pipeline([
             ('vect', extraction.CountVectorizer()),
             ('chi2', fSelection.SelectFpr(fSelection.chi2, alpha=a)),
-            ('clf', neural.MLPClassifier(solver='adam',early_stopping=True, max_iter=400)),
+            ('clf', neural.MLPClassifier()),
         ])
         runTest(pipe, split, "alpha = " + str(a))
-    
+
+if __name__ == "__main__":
+
+    data = loadFiles()
+    split = splitData(data)
+    proc = []
+
+    print("\nCount Vect -> SelectFpr -> MLP")
+    pipe = Pipeline([
+        ('vect', extraction.CountVectorizer(analyzer=stemmed_words)),
+        ('chi2', fSelection.SelectFwe(fSelection.chi2, alpha=.1)),
+        ('clf', neural.MLPClassifier(solver='adam',early_stopping=True, max_iter=400)),
+    ])
+    runTest(pipe, split, "english")
 
     #My test pipelines 
     '''
